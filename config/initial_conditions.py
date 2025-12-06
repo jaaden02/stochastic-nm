fb4fab818cfeec9afd9619812272556d0810121e
Jasper Aden <jaaden@ethz.ch>
2025-12-06 19:03:45 +0100

    New stuff

---
+++ config/initial_conditions.py
@@ -0,0 +1,17 @@
+z0 = -2
+x0 = 0
+phi0 = -0.5*np.pi
+
+sigma_x   = 0.1 # 2.0 * dx    
+sigma_z   =  2.0 * dz    
+sigma_phi =  2.0 * dphi  
+
+# Grid:
+# X in 0, 1200
+# Z in 0, -4
+
+# Nz = 
+# Nx = 
+# Nphi =
+# t_final = 3000
+# dt = 0.1
